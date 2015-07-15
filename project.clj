f47093196c56872d5dd9d1732230581e97772a3c
Divyansh Prakash <divyansh@redpanda.co.in>
2015-07-16 01:44:37 +0530

    initial

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject snake-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [quil "2.2.6"]
+                 [org.clojure/clojurescript "0.0-3308"]]
+
+  :plugins [[lein-cljsbuild "1.0.6"]]
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds [{:source-paths ["src"]
+             :compiler
+             {:output-to "js/main.js"
+              :output-dir "out"
+              :main "snake_cljs.core"
+              :optimizations :none
+              :pretty-print true}}]})
